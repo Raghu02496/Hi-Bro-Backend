e2db8f6b8edf981ed7a99fcd44855e18eb5ed4ba
R <raghuramap41@gmail.com>
2025-05-08 11:56:52 +0530

    removed logs

--- routes.js
+++ routes.js
@@ -1,12 +1,9 @@
 function listenToApi(app){
     app.get('/status',(request,response)=>{
-        console.log('request recieved');
         response.json({ok : true , data : "Everything ok nigga"})
     })
     
     app.post('/info',(request,response)=>{
-        console.log(request.headers,'request headers')
-        console.log(request.body,'request body')
         response.json({ok:true , data : request.body})
     })
 }
