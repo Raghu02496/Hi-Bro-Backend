2c68f5509b169c1be0425b2c30e476f4cb28b018
Raghu02496 <lawlieta023@gmail.com>
2025-05-12 22:19:50 +0530

    startup server after mongodb connection is made

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors')
+const connectMongo = require('./connectMongo')
 const apiListeners = require('./routes')
 
 const app = express();
@@ -9,8 +10,10 @@ app.use(cors({
     origin: 'http://127.0.0.1:5500'
 }));
 
-app.listen(3000, () => {
-    console.log("Server Listening on PORT: 3000");
+connectMongo().then(()=>{
+    app.listen(3000, () => {
+        console.log("Server Listening on PORT: 3000");
+    });
+    
+    apiListeners(app);
 });
-
-apiListeners(app);
