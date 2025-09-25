536dd50e2694217c1aee167090e68652e78e282d
Raghu02496 <lawlieta023@gmail.com>
2025-09-25 07:42:22 +0530

    [MNT] moved model fn to mongo.js , router grouping added

--- routes.js
+++ routes.js
@@ -1,26 +1,17 @@
-const fs = require('fs');
-const path = require('path');
-const mongoose = require('mongoose')
-const toDoSchema = require('./models')
+const express = require('express');
+const router = express.Router();
+const mongo = require('./mongo')
 
-function listenToApi(app){
-    app.get('/status',(request,response)=>{
+function listenToApi(){
+    router.get('/status',(request,response)=>{
         response.json({ok : true , data : "Everything ok"})
     })
 
-    app.post('/getCat',(request,response)=>{
-        const filePath = path.join(__dirname, 'files', 'cat.jpg');
-        const readStream = fs.createReadStream(filePath);
-        response.setHeader('Content-Type', 'image/jpeg');
-        readStream.pipe(response);
-    })
-
-    app.post('/addTodo',(request,response)=>{
+    router.post('/addTodo',(request,response)=>{
         const {done, string } = request.body;
 
         try{
-            const TodoModel = mongoose.model('todo',toDoSchema,'todo_collection');
-            const todo = new TodoModel({string : string ,done : done})
+            const todo = new mongo.TodoModel({string : string ,done : done})
             todo.save();
             response.json({ok : true , msg : 'todo added'})
         }catch(error){
@@ -28,12 +19,11 @@ function listenToApi(app){
         }
     })
 
-    app.post('/getTodo',async (request,response)=>{
+    router.post('/getTodo',async (request,response)=>{
         const {limit, page } = request.body;
         
         try{
-            const TodoModel = mongoose.model('todo',toDoSchema,'todo_collection');
-            const todos = await TodoModel.find().skip(parseInt(limit*page)).limit(parseInt(limit));
+            const todos = await mongo.TodoModel.find().skip(parseInt(limit*page)).limit(parseInt(limit));
             response.json({ok : true , todo : todos })
         }catch(error){
             response.status(500).json({ok : false , error : error})
@@ -42,4 +32,4 @@ function listenToApi(app){
 
 }
 
-module.exports = listenToApi
+module.exports = {listenToApi,router}
