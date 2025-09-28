acb96ba6279dd8eea05729f6ab5da700646cf5b5
Raghu <raghuramap41@gmail.com>
2025-09-28 12:24:58 +0530

    route controllers moved to controllers.js

---
+++ controllers.js
@@ -0,0 +1,34 @@
+const mongo = require('./mongo')
+
+function sendStatus(request, response) {
+    response.json({ ok: true, data: "Everything ok" })
+}
+
+function addTodo(request, response) {
+    const { done, string } = request.body;
+
+    try {
+        const todo = new mongo.TodoModel({ string: string, done: done })
+        todo.save();
+        response.status(201).json({ ok: true, msg: 'todo added' })
+    } catch (error) {
+        response.status(500).json({ ok: false, error: error })
+    }
+}
+
+async function getTodo(request, response) {
+    const { limit, page } = request.body;
+
+    try {
+        const todos = await mongo.TodoModel.find().skip(parseInt(limit * page)).limit(parseInt(limit));
+        response.json({ ok: true, todo: todos })
+    } catch (error) {
+        response.status(500).json({ ok: false, error: error })
+    }
+}
+
+module.exports = {
+    sendStatus,
+    addTodo,
+    getTodo
+}
--- routes.js
+++ routes.js
@@ -1,35 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const mongo = require('./mongo')
+const controllers = require('./controllers')
 
 function listenToApi(){
-    router.post('/status',(request,response)=>{
-        response.json({ok : true , data : "Everything ok"})
-    })
-
-    router.post('/addTodo',(request,response)=>{
-        const {done, string } = request.body;
-
-        try{
-            const todo = new mongo.TodoModel({string : string ,done : done})
-            todo.save();
-            response.status(201).json({ok : true , msg : 'todo added'})
-        }catch(error){
-            response.status(500).json({ok : false , error : error})
-        }
-    })
-
-    router.post('/getTodo',async (request,response)=>{
-        const {limit, page } = request.body;
-        
-        try{
-            const todos = await mongo.TodoModel.find().skip(parseInt(limit*page)).limit(parseInt(limit));
-            response.json({ok : true , todo : todos })
-        }catch(error){
-            response.status(500).json({ok : false , error : error})
-        }
-    })
-
+    router.post('/status',controllers.sendStatus)
+    router.post('/addTodo',controllers.addTodo)
+    router.post('/getTodo',controllers.getTodo)
 }
 
 module.exports = {listenToApi,router}
