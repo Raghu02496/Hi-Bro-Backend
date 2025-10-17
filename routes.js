90f4d3050551bd868b89b09ff449387e3f38d80c
Raghu02496 <lawlieta023@gmail.com>
2025-10-17 23:57:18 +0530

    routes added for fetching and sending content

--- routes.js
+++ routes.js
@@ -1,13 +1,9 @@
 import express from "express"
-import { sendStatus,addTodo,getTodo,updateTodoById,deleteTodoById,msgChatGpt } from "./controllers.js"
+import { msgChatGpt,getConversation } from "./controllers.js"
 
 export const router = express.Router();
 
 export function listenToApi(){
-    router.post('/status',sendStatus)
-    router.post('/addTodo',addTodo)
-    router.post('/getTodo',getTodo)
-    router.post('/updateTodoById',updateTodoById)
-    router.post('/deleteTodoById',deleteTodoById)
     router.post('/msgChatGpt',msgChatGpt)
+    router.get('/getConversation',getConversation)
 }
