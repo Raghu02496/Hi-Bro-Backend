04fd84c5f52b7c1af1a64c37903e279b996f3101
Raghu02496 <lawlieta023@gmail.com>
2025-10-19 22:36:16 +0530

    summarizing after every 10 messages to reduce token usage

--- controllers.js
+++ controllers.js
@@ -1,5 +1,5 @@
 import { OpenAI } from "openai"
-import { messageModel } from "./mongo.js";
+import { messageModel, caseModel } from "./mongo.js";
 
 export function sendStatus(request, response) {
     response.json({ ok: true, data: "Everything ok" })
@@ -13,13 +13,25 @@ export async function msgChatGpt(request, response) {
     try {
         const { content, case_id } = request.body
 
-        let previousConversations = await messageModel.find({case_id : case_id })
+        let curCase = await caseModel.findOne({_id : case_id})
+
+        // fetch the recent 10 messages
+        let previousConversations = await messageModel.find({case_id : case_id }).skip(curCase.lastSummaryCount).limit(10)
+
+        //count the total number of messages
+        let totalCount = await messageModel.countDocuments({case_id : case_id })
+
+        if(totalCount - curCase.lastSummaryCount >= 10 ){
+            curCase.summary = await generateSummary(previousConversations,curCase,openai)
+            previousConversations = []
+        }
 
         previousConversations.push({ role: "user", content: content })
-
+        
         const completion = await openai.chat.completions.create({
             model: "gpt-4o-mini",
             messages: [
+                {role : 'system', content : `previous coversations summary ${curCase.summary}`},
                 ...previousConversations
             ]
         });
@@ -45,4 +57,26 @@ export async function getConversation(request, response){
     }catch(error){
         response.status(500).json({ ok: false, error: error })
     }
+}
+
+async function generateSummary(previousConversations,curCase,openai){
+
+    const completion = await openai.chat.completions.create({
+        model: "gpt-4o-mini",
+        messages: [
+            {
+                role : 'system',
+                content : `Summarize and compress this conversation as much possible without losing key details
+                ${JSON.stringify(previousConversations)}`
+            }
+        ]
+    });
+
+    const reply = completion.choices[0].message.content;
+
+    const newSummary = curCase.summary ? (curCase.summary+'\n'+reply) : reply
+
+    await caseModel.updateOne({_id : curCase._id},{$set : {summary : newSummary},$inc : {lastSummaryCount : 10}})
+
+    return newSummary
 }
