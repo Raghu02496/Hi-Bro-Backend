6d0532cc67ef5405838ae40307c5a660281a5934
Raghu02496 <lawlieta023@gmail.com>
2025-10-19 12:14:13 +0530

    schema changes - message and case are kept in seperate collection

--- models.js
+++ models.js
@@ -1,6 +1,13 @@
 import { mongoose } from "mongoose"
 
-export const conversationSchema = new mongoose.Schema({
+export const caseSchema = new mongoose.Schema({
+    _id : String,
+    lastSummaryCount : Number,
+    summary : String
+})
+
+export const messageSchema = new mongoose.Schema({
+    case_id : String,
     role : String,
     content : String
 })
