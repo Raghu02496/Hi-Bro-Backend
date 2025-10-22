1e6d92fcf4843a78e4d2d12a4e4d9680e45da115
Raghu02496 <lawlieta023@gmail.com>
2025-10-22 18:10:59 +0530

    status api added

--- routes.js
+++ routes.js
@@ -1,9 +1,10 @@
 import express from "express"
-import { msgChatGpt,getConversation,generateCase,getCaseById } from "./controllers.js"
+import { msgChatGpt, getConversation, generateCase, getCaseById, sendStatus } from "./controllers.js"
 
 export const router = express.Router();
 
 export function listenToApi(){
+    router.post('/status',sendStatus)
     router.post('/msgChatGpt',msgChatGpt)
     router.post('/getConversation',getConversation)
     router.post('/generateCase',generateCase)
